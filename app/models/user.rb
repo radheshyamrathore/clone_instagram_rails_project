2e9302ed18bf75d0a8aa5e58474c217d3121bb38
radheshyamrathore <rsrathore002@gmail.com>
2023-01-10 11:27:22 +0530

    made some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
     has_many :comments,through: :posts
     has_many :likes,through: :posts
     validates :user_name,uniqueness: true
-    validates :gender,numericality: true
 end
 
